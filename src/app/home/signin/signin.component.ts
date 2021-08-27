0e311bf3df575d68e8a78dbdfb5741a7b05b7390
LuigiToniolo <luigi.toniolo@domvsit.com.br>
2021-08-27 11:32:10 -0300

    Finalizando o projeto Alurapic

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../core/auth/auth.service';
@@ -9,40 +10,49 @@ import { Title } from '@angular/platform-browser';
     templateUrl: './signin.component.html'
 })
 export class SignInComponent implements OnInit {
-    
+
+    fromUrl: string;
     loginForm: FormGroup;
     @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
-    
+
     constructor(
         private formBuilder: FormBuilder,
         private authService: AuthService,
         private router: Router,
-        private platformDetectorService: PlatformDetectorService) { }
+        private platformDetectorService: PlatformDetectorService,
+        private activatedRoute: ActivatedRoute) { }
 
     ngOnInit(): void {
+        this.activatedRoute
+            .queryParams
+            .subscribe(params => this.fromUrl = params['fromUrl']);
+
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
         });
-        this.platformDetectorService.isPlatformBrowser() && 
-        this.userNameInput.nativeElement.focus();        
-    } 
+        this.platformDetectorService.isPlatformBrowser() &&
+        this.userNameInput.nativeElement.focus();
+    }
 
     login() {
-        const userName = this.loginForm.get('userName').value;
-        const password = this.loginForm.get('password').value;
+      const userName = this.loginForm.get('userName').value;
+      const password = this.loginForm.get('password').value;
 
-        this.authService
-            .authenticate(userName, password)
-            .subscribe(
-                () => this.router.navigate(['user', userName]),
-                err => {
-                    console.log(err);
-                    this.loginForm.reset();
-                    this.platformDetectorService.isPlatformBrowser() && 
-                        this.userNameInput.nativeElement.focus();
-                    alert('Invalid user name or password');
-                }
-            );
-    }
+      this.authService
+          .authenticate(userName, password)
+          .subscribe(
+              () => this.fromUrl
+                  ? this.router.navigateByUrl(this.fromUrl)
+                  : this.router.navigate(['user', userName])
+              ,
+              err => {
+                  console.log(err);
+                  this.loginForm.reset();
+                  this.platformDetectorService.isPlatformBrowser() &&
+                      this.userNameInput.nativeElement.focus();
+                  alert('Invalid user name or password');
+              }
+          );
+  }
 }
